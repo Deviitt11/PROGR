92b7284c749d8e023465f7fdeadb159d96961aed
Deviitt11 <david.collados11@gmail.com>
2024-04-18 13:52:15 +0200

    18 Abril

---
+++ Tema 6/MVCExample/src/modelos/Estudiante.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author ALUMNOS_FP
+ */
+public class Estudiante {
+    
+    String name;
+    String dni;
+    int edad;
+
+    public Estudiante(String name, String dni, int edad) {
+        this.name = name;
+        this.dni = dni;
+        this.edad = edad;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
