b7228c2327431989fe54a6353fe73a43ee83396f
Deviitt11 <david.collados11@gmail.com>
2024-04-24 13:47:33 +0200

    24 Abril

---
+++ Tema 6/EJ6/src/animal/Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animal;
+
+/**
+ *
+ * @author ALUMNOS_FP
+ */
+public abstract class Animal implements Animal {
+    
+    
+    
+}
---
+++ Tema 6/EJ6/src/ej6/EJ6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej6;
+
+/**
+ *
+ * @author ALUMNOS_FP
+ */
+public class EJ6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
