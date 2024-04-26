9d5447413f9527e252a5cf9c01bf2ee056e1dcba
Deviitt11 <david.collados11@gmail.com>
2024-04-26 13:09:43 +0200

    26 Abril

--- Tema 6/Factorial/src/factorial/Factorial.java
+++ Tema 6/Factorial/src/factorial/Factorial.java
@@ -2,19 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package david_collados_blanco_prog_tarea6;
+package factorial;
 
 /**
  *
  * @author ALUMNOS_FP
  */
-public class David_Collados_Blanco_PROG_TAREA6 {
+public class Factorial {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
+    public static int factorialIterativo(int num) {
+        
+        int factorial = 1;
+        
+        for (int i = 0; i < 10; i++) {
+            factorial *= num;
+        }
+        
+        return factorial;
+    }
+    
+    public static int factRecur(int num) {
+        
+    }
 }
