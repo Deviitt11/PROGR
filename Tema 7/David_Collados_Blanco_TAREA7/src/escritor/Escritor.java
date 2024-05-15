e494bfe187f3989139eac5d6c9ed788dffebe55c
Deviitt11 <david.collados11@gmail.com>
2024-05-15 11:32:51 +0200

    15 Mayo

--- Tema 7/David_Collados_Blanco_TAREA7/src/escritor/Escritor.java
+++ Tema 7/David_Collados_Blanco_TAREA7/src/escritor/Escritor.java
@@ -4,9 +4,34 @@
  */
 package escritor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Escritor {
     
     public static void escribirFichero(String fileName, String contenido) {
+        try {
+            
+            File file = new File(ruta);
+            FileWriter shake = new FileWriter(file, true);
+            
+            // escribimos
+            shake.write(' ');
+            shake.write("\n");
+            shake.write("mensaje: \n");
+            shake.write("COMUNICADO OFICIAL: Kylian Mbappé\n\n");
+            shake.write("El club se complace de anunciar el fichaje del astro francés\n\n");
+            
+            shake.close();
+            
+            
+        } catch (FileNotFoundException ex) {
+            System.err.println("FileNotFoundException: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("IOException capturado");
+        }
     }
     
     public static void escribirNumeros(String fileName, ArrayList<Integer>) {
--- Tema 7/David_Collados_Blanco_TAREA7/src/lector/Lector.java
+++ Tema 7/David_Collados_Blanco_TAREA7/src/lector/Lector.java
@@ -9,5 +9,53 @@ package lector;
  * @author ALUMNOS_FP
  */
 public class Lector {
+
+    // Atributo
+    private String nombreFichero = "ficheroPrueba.txt";
+
+    // Setters y getters
+    public String getNombreFichero() {
+        return nombreFichero;
+    }
+
+    public void setNombreFichero(String nombreFichero) {
+        this.nombreFichero = nombreFichero;
+    }
+
+    // Constructor
+    public Lector() {
+    }
+    
+
+    // Métodos
+    public static int buscarPalabra(String palabraBuscada) {
+        int resultado = 0;
+        
+        return resultado;
+    }
+
+    public static int contarPalabra(String palabraBuscada) {
+        int resultado = 0;
+
+        return resultado;
+    }
+
+    public static int contarVocales(){
+        int contador = 0;
+        
+        return contador;
+    }
+
+    public static void leerFicheroCifrado(){
+    
+    }
+    
+    public static void leerFicheroDelimitado(){
+        
+    }
+    
+    
+    
+    
     
 }
